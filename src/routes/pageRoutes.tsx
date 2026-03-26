b982d901ab079fb022275e0155d2baaa6cf22193
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-03-26 09:52:27 +0500

    feat: #52: add permission for invoices routes

--- src/routes/pageRoutes.tsx
+++ src/routes/pageRoutes.tsx
@@ -1,14 +1,16 @@
 import { BreadcrumbComponentProps } from 'use-react-router-breadcrumbs'
 import { invoicesRoutes } from '../pages/routes'
 
-export function getPageRoutes() {
+export function getPageRoutes(accessPermissions: Map<any, boolean>) {
   const routes: {
     path: string,
     breadcrumb: string | ((props: BreadcrumbComponentProps) => string | undefined),
     Component: () => JSX.Element,
   }[] = []
 
-  routes.push(invoicesRoutes)
+  if (accessPermissions.get(`CanViewInvoices`)) {
+    routes.push(invoicesRoutes)
+  }
 
   return routes
 }
